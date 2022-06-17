5decff3c024eceb9d5ecac63614056ed1500b261
Pablo Valle <pablo.valle@crmservices.com.br>
2022-06-16 23:48:02 -0300

    Finalizado modulo IV

---
+++ src/modules/cars/useCases/createCategory/CreateCategoryController.spec.ts
@@ -0,0 +1,71 @@
+import request from "supertest";
+import { app } from "@shared/infra/http/app";
+import { v4 as uuidV4 } from "uuid";
+import createConnection from "@shared/infra/typeorm";
+import { Connection } from "typeorm";
+import { hash } from "bcryptjs";
+
+let connection: Connection;
+
+describe("Create Category Controller", () => {
+  beforeAll(async () => {
+    connection = await createConnection();
+    await connection.runMigrations();
+
+    const id = uuidV4();
+    const password = await hash("admin", 8);
+
+    await connection.query(
+      `INSERT INTO USERS (id, name, email, password, "isAdmin", created_at, driver_license) 
+      VALUES ('${id}', 'Admin', 'admin@rentx.com.br', '${password}', true,  'now()', 'AAA-BBBB')
+      `
+    );
+  });
+
+  afterAll(async () => {
+    await connection.dropDatabase();
+    await connection.close();
+  });
+
+  it("Should be able to create a new category", async () => {
+    const responseToken = await request(app).post("/sessions").send({
+      email: "admin@rentx.com.br",
+      password: "admin",
+    });
+
+    const { token } = responseToken.body;
+
+    const response = await request(app)
+      .post("/categories")
+      .send({
+        name: "Category Supertest",
+        description: "category supertest",
+      })
+      .set({
+        Authorization: `Bearer ${token}`,
+      });
+
+    expect(response.status).toBe(201);
+  });
+
+  it("Should not be able to create a new category with name exists", async () => {
+    const responseToken = await request(app).post("/sessions").send({
+      email: "admin@rentx.com.br",
+      password: "admin",
+    });
+
+    const { token } = responseToken.body;
+
+    const response = await request(app)
+      .post("/categories")
+      .send({
+        name: "Category Supertest",
+        description: "category supertest",
+      })
+      .set({
+        Authorization: `Bearer ${token}`,
+      });
+
+    expect(response.status).toBe(400);
+  });
+});
--- src/shared/container/index.ts
+++ src/shared/container/index.ts
@@ -4,11 +4,14 @@ import { ICategoriesRepository } from "@modules/cars/repositories/ICategoriesRep
 import { CategoriesRepository } from "@modules/cars/infra/typeorm/repositories/CategoriesRepository";
 import { ISpecificationsRepository } from "@modules/cars/repositories/ISpecificationsRepository";
 import { container } from "tsyringe";
+import "@shared/container/providers";
 import { SpecificationsRepository } from "@modules/cars/infra/typeorm/repositories/SpecificationsRepository";
 import { ICarsRepository } from "@modules/cars/repositories/ICarsRepository";
 import { CarsRepository } from "@modules/cars/infra/typeorm/repositories/CarsRepository";
 import { ICarsImagesRepository } from "@modules/cars/repositories/ICarsImagesRepository";
 import { CarsImagesRepository } from "@modules/cars/infra/typeorm/repositories/CarsImagesRepository";
+import { IRentalsRepository } from "@modules/rentals/repositories/IRentalsRepository";
+import { RentalsRepository } from "@modules/rentals/infra/typeorm/repositories/RentalsRepository";
 
 // ICategoriesRepository
 container.registerSingleton<ICategoriesRepository>(
@@ -31,4 +34,9 @@ container.registerSingleton<ICarsRepository>("CarsRepository", CarsRepository);
 container.registerSingleton<ICarsImagesRepository>(
   "CarsImagesRepository",
   CarsImagesRepository
+);
+
+container.registerSingleton<IRentalsRepository>(
+  "RentalsRepository",
+  RentalsRepository
 );
